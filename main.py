b3bc625ee6a33bea92e111f4b64457abfcdb562a
mallikarjunreddyd <arjun753016@gmail.com>
2024-10-19 21:16:18 +0530

    Create environment and Add packages - Seond Commit

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 def main():
     ## 1. Environment Setup 
+        ## Install Python 3.8
+        ## Install miniconda (https://docs.anaconda.com/miniconda/)
+        ## Activate miniconda (source miniconda3/bin/activate)
+        ## Create Virtual environment using Conda (conda create -n flower_tutorial python=3.8 -y)
+        ## Activate FLTutorial (conda activate flower_tutorial)
+        ## Select Framework (Pytorch) (  conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda==11.6 -c pytorch -c nvidia -y)
+        ## Install Flower Framework (pip install flwr==1.4.0)
+        ## Install Hydra (pip install hydra-core)
+        ## Install Ray (pip install ray==1.11.1)
+
 
     ## 2. Dataset preparation
 
