5cc0132d159e4cf24390308600921820f301987a
mallikarjunreddyd <arjun753016@gmail.com>
2024-10-19 20:25:29 +0530

    Add main - First Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+def main():
+    ## 1. Environment Setup 
+
+    ## 2. Dataset preparation
+
+    ## 3. Client Setup
+
+    ## 4. FL Strategy and Server Setup
+
+    ## 5. FL Simulation
+
+    ## 6. Saving results
+
+if __name__ == "__main__":
+    main()
